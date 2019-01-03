9525621029e82f575ebc5380d92a5fe225f97052
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2019-01-04 02:45:37 +0800

    Refactored App.js. Removed unescessary codes.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <h1>A MERN To-Do App</h1>
       </div>
     );
   }
