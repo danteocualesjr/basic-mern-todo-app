865e859492c40cc67c245e50b5f032e6931e98a5
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2019-01-05 23:43:53 +0800

    Added nav in App.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,11 @@ class App extends Component {
     return (
       <Router>
         <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="https://www.lambdaschool.com" target="_blank">
+              <img src={logo} width="30" height="30" alt="www.lambdaschool.com" />
+            </a>
+            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
           <h1>A MERN To-Do App</h1>
         </div>
         <Route path="/" exact component={TodoList} />
