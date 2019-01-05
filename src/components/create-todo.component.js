ad0ab6563ae5c36895cb74ecda18e2fe0161c990
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2019-01-05 23:31:17 +0800

    Configured CreateTodo as a class component.

---
+++ src/components/create-todo.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CreateTodo extends from Component {
+    render() {
+        return (
+            <div>
+                <p>CreateTodo</p>
+            </div>
+        )
+    }
+}
+
+export default CreateTodo;
