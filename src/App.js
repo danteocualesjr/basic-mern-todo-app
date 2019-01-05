f9a3bb21ba3b52b704780260d151c749da0d90b7
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2019-01-05 23:39:27 +0800

    Configured App.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import CreateTodo from './components/create-todo.components.js';
-import EditTodo from './components/edit-todo.components.js';
-import TodosList from './components/todo-list.components.js';
+import CreateTodo from "./components/create-todo.components.js";
+import EditTodo from "./components/edit-todo.components.js";
+import TodosList from "./components/todo-list.components.js";
+
+import logo from "./logo.png";
 
 class App extends Component {
   render() {
@@ -13,10 +15,10 @@ class App extends Component {
       <Router>
         <div className="container">
           <h1>A MERN To-Do App</h1>
-          <Route path="/" exact component={TodoList} />
-          <Route path="/edit/:id" component={EditTodo} />
-          <Route path="/create" component={CreateTodo} />
         </div>
+        <Route path="/" exact component={TodoList} />
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
       </Router>
     );
   }
