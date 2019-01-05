e7141eab54e8460e8454af6eefd20249632950a5
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2019-01-05 23:34:19 +0800

    Imported the three components in App.js:
    1.  CreateTodo
    2. EditTodo
    3. TodosList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from './components/create-todo.components.js';
+import EditTodo from './components/edit-todo.components.js';
+import TodosList from './components/todo-list.components.js';
 
 class App extends Component {
   render() {
