7ed31e55394ac60c565537e2b5f2feaa2f72ecce
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2019-01-05 23:30:27 +0800

    Configured EditTodo as a class component.

---
+++ src/components/edit-todo.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EditTodo extends from Component {
+    render() {
+        return (
+            <div>
+                <p>EditTodo</p>
+            </div>
+        )
+    }
+}
+
+export default EditTodo;
