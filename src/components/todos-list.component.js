243690951d732db46f5e32656b6033d7062ccc2f
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2019-01-05 23:29:26 +0800

    Configured TodosList as a class component.

---
+++ src/components/todos-list.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodosList extends from Component {
+    render() {
+        return (
+            <div>
+                <p>TodosList</p>
+            </div>
+        )
+    }
+}
+
+export default TodosList;
