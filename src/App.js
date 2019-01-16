2cd1af5541d8b2fe65cdc8d5a856e43780e61b0e
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2019-01-16 13:54:55 +0800

    Corrected typose in name of components.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import CreateTodo from "./components/create-todo.components.js";
-import EditTodo from "./components/edit-todo.components.js";
-import TodoList from "./components/todo-list.components.js";
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodoList from "./components/todo-list.component";
 
 import logo from "./logo.png";
 
